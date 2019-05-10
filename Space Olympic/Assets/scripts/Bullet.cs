b6c8a9edb025c4be8fdefe8b731a60c1837426e2
koukemo <java.sparrow22.surface@gmail.com>
2019-05-10 17:12:20 +0900

    first commit

---
+++ Space Olympic/Assets/scripts/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class Bullet : MonoBehaviour { 
+    [SerializeField] float speed = 20f; //弾速 [m/s]
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Space Olympic/Assets/scripts/Shooter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+    [SerializeField] GameObject bulletPrefabs;  //弾丸のプレハブ
+    [SerializeField] Transform gunBarrelEnd;    //銃口位置
+
+    // Update is called once per frame
+    void Update() {
+        //enterが押されたとき発砲する
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        //弾丸プレハブを元に戻し、シーン上に再生成
+        Instantiate(bulletPrefabs, gunBarrelEnd.position, gunBarrelEnd.rotation);
+    }
+}
